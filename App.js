3ef336b993d69a695dff360fa4d16881c2efbd26
w0nght <wwongheitung@gmail.com>
2019-04-24 22:12:58 +1000

    Added react-navigator to app js

--- App.js
+++ App.js
@@ -6,75 +6,30 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import SplashScreen from './src/screens/SplashScreen';
 import Home from './src/screens/Home';
+import Cards from './src/screens/Cards';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
-// https://expo.github.io/vector-icons/
+import { createStore } from 'redux';
 
+// const reducer = () => {
+
+// }
+// const store = createStore(reducer);
 export default class App extends Component {
-  // create constructor for App component 
-  // set an initial state
-  constructor(props) {
-    super(props);
-  
-    this.state = { isLoading: true};
-  }
-
-    // define a function which resolves itself in 2 seconds
-  performTimeConsumingTask = async() => {
-    return new Promise((resolve) =>
-      setTimeout(
-        () => { resolve('result') },
-        2000  // 2 seconds
-        )
-      );
-    }
-
-    // define a callback function componentDidMount
-    async componentDidMount() {
-      // Preload data from an external API
-      // Preload data using AsyncStorage
-      const data = await this.performTimeConsumingTask()
-
-      if (data !== null) {
-        // when we get the result back, 
-        //we are going to change isLoading state to false
-        // That will remove the splash screen and renders the application itself
-        this.setState({ isLoading: false });
-        // this.props.navigation.navigate('App');
-      }
-    }
-
   render() {
-    if (this.state.isLoading) {
-      return <SplashScreen />;
-    }
     return (
-      <Home />
+      <AppContainer />
     );
   }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const AppStackNavigator = createStackNavigator({
+  Home: Home,
+  Cards: Cards
 });
+
+const AppContainer = createAppContainer(AppStackNavigator);
