1e0024ff03037c408d9282628bb9fd33ca7e8bf1
w0nght <wwongheitung@gmail.com>
2019-04-25 23:13:27 +1000

    Added Images.js for easy manage all images on this project

---
+++ src/assets/Images.js
@@ -0,0 +1,11 @@
+/* Images.js */
+/* Controls all images from one file */
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+
+const images = {
+	broShakeLogo: require('./images/Bro_Shake_Logo_Green.png')
+};
+
+
+export default images;
