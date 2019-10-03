fe14958c547a755805c91de692515a99c3f324e1
w0nght <wwongheitung@gmail.com>
2019-10-03 18:59:11 +1000

    Merged Terms and Conditions footer in Auth Screen.

---
+++ Components/TermsFooter.js
@@ -0,0 +1,49 @@
+/*TermsFooter.js*/
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, } from 'react-native';
+import fonts from '../src/assets/Fonts';
+
+class TermsFooter extends Component {
+  render() {
+    return (
+			<View style={styles.footerTextCont}>
+				<Text style={styles.instructions}>By signing up, you agree to FanTipper's</Text>
+					<View style={styles.footerBtnCont}>
+						<TouchableOpacity>
+							<Text style={styles.footerBtn}>T&C</Text>
+						</TouchableOpacity>
+						<Text style={styles.instructions}>'s and </Text>
+						<TouchableOpacity>
+							<Text style={styles.footerBtn}>Privacy Policy</Text>
+						</TouchableOpacity>
+						<Text style={styles.instructions}>.</Text>
+					</View>
+			</View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+	footerTextCont: {
+		alignItems: 'center',
+	},
+	footerBtnCont: {
+		flexDirection: 'row',
+	},
+	instructions: {
+		fontFamily: fonts.Larsseit,
+		fontSize: 18,
+		color: '#939393',
+		lineHeight: 26
+	},
+	footerBtn: {
+		fontFamily: fonts.Larsseit,
+		fontSize: 18,
+		color: '#939393',
+		textDecorationLine: 'underline',
+	},
+});
+
+
+export default TermsFooter;
