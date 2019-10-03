1e26e686601de98104dd727cd61af40f574a1483
w0nght <wwongheitung@gmail.com>
2019-10-03 18:59:29 +1000

    Updated

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,14 +2,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import fonts from '../assets/Fonts';
-
-import SplashScreen from './SplashScreen';
 import Cards from './Cards';
 
-// https://expo.github.io/vector-icons/
-
-// https://www.robinwieruch.de/react-warning-cant-call-setstate-on-an-unmounted-component/
-// to solve the unmount componment
 class Home extends Component {
   render() {
     return (
@@ -24,14 +18,6 @@ class Home extends Component {
           <Cards imageUri={require('../assets/images/cprofile_1.jpg')} distance='34' navigation={this.props.navigation}/>
           <Cards imageUri={require('../assets/images/cprofile_1.jpg')} distance='20' />
           <Cards imageUri={require('../assets/images/cprofile_3.jpg')} distance='10' />
-          {/* <Content>
-          <CardComponent imageSource='1' navigation={this.props.navigation}/>
-            {/* <CardComponent navigation={this.props.navigation} imageSource='1'/> */}
-            {/* <CardComponent imageSource='2'/> */}
-            {/* <CardComponent imageSource='3'/> */}
-            {/* <CardComponent imageSource='2'/> */}
-          {/* </Content> */}
-          {/* <Cards navigation={this.props.navigation} /> */}
         </ScrollView>
       </View>
     );
